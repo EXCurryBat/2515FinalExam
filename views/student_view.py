6b5de003ba70b28305b666b6b9bc04c70bc3ccba
EXcurrybat <dxiao5@my.bcit.ca>
2018-04-19 19:18:33 -0700

    a

--- views/student_view.py
+++ views/student_view.py
@@ -16,10 +16,14 @@ class StudentView():
         self.top_frame = Frame(self.sub)
         self.mid_frame = Frame(self.sub)
         self.bot_frame = Frame(self.sub)
+        self.right_frame = Frame(self.sub)
+        self.bot_right_frame = Frame(self.sub)
 
-        self.top_frame.grid(row=0, padx=30, pady=10)
-        self.mid_frame.grid(row=1, padx=30, pady=10)
-        self.bot_frame.grid(row=2, padx=30, pady=10)
+        self.top_frame.grid(row=0,column=0, padx=30, pady=10)
+        self.mid_frame.grid(row=1,column=0, padx=30, pady=10)
+        self.bot_frame.grid(row=2,column=0, padx=30, pady=10)
+        self.right_frame.grid(row=0,column=1,rowspan=2, padx=30,pady=10)
+        self.bot_right_frame.grid(row=3, column=1,padx=30,pady=10)
 
         # widgets
         self.stu_id = Label(self.top_frame, text='')
@@ -39,6 +43,21 @@ class StudentView():
         self.electives.pack()
         self.elective_listbox.pack(side = LEFT,fill=BOTH)
         self.elective_scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.delete_button = Button(self.bot_frame, text='delete', width=10)
+        self.delete_button.pack()
+
+        self.elec_option_text = Label(self.right_frame, text='Available electives: ')
+        self.elec_option_listbox = Listbox(self.right_frame, width=20, selectmode=SINGLE)
+        self.elec_option_scrollbar = Scrollbar(self.right_frame, orient='vertical')
+        self.elec_option_scrollbar.config(command=self.elec_option_listbox.yview)
+        self.elec_option_listbox.config(yscrollcommand=self.elec_option_scrollbar.set)
+        self.elec_option_text.pack()
+        self.elec_option_listbox.pack(side=LEFT, fill=BOTH)
+        self.elec_option_scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.add_button = Button(self.bot_right_frame, text='Add', width=10)
+        self.add_button.pack()
 
 
     def unpack(self):
